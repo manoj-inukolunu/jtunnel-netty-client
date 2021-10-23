0fd86d194f9a216ae2106fdd0459736f0a14b81a
Manoj Inukolunu <minukolunu@axiamatic.com>
2021-10-23 13:16:35 +0530

    Use Tunnel Config from the command line

--- src/main/java/com/jtunnel/netty/TunnelConfig.java
+++ src/main/java/com/jtunnel/netty/TunnelConfig.java
@@ -2,13 +2,9 @@ package com.jtunnel.netty;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "tunnel")
 @Getter
 @Setter
-@Configuration
 public class TunnelConfig {
 
   private String destHost;
--- src/main/java/com/jtunnel/spring/HttpServer.java
+++ src/main/java/com/jtunnel/spring/HttpServer.java
@@ -3,6 +3,9 @@ package com.jtunnel.spring;
 
 import com.jtunnel.data.DataStore;
 import com.jtunnel.data.MapDbDataStore;
+import com.jtunnel.netty.TunnelConfig;
+import java.util.Objects;
+import java.util.Properties;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @Log
 @SpringBootApplication
@@ -19,14 +23,37 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class HttpServer {
 
-
   public static void main(String args[]) throws Exception {
     log.info("Starting Http Server");
-    new SpringApplication(HttpServer.class).run(args);
+    if (args.length != 4) {
+      System.out.println(
+          "Usage : java -jar jTunnel.jar <destination-host> <destination-port> <subdomain> <db storage location>");
+      return;
+    }
+    SpringApplication application = new SpringApplication(HttpServer.class);
+    Properties properties = new Properties();
+    properties.put("server.port", "5050");
+    properties.put("tunnel.dest-host", args[0]);
+    properties.put("tunnel.dest-port", args[1]);
+    properties.put("tunnel.server-host", args[2]);
+    properties.put("tunnel.server-port", 1234);
+    properties.put("data-store-location", args[3]);
+    application.setDefaultProperties(properties);
+    application.run(args);
   }
 
   @Bean
-  public DataStore dataStore(@Value("${data-store-location}") String dir) {
-    return new MapDbDataStore(dir);
+  public TunnelConfig tunnelConfig(Environment environment) {
+    TunnelConfig tunnelConfig = new TunnelConfig();
+    tunnelConfig.setServerHost(environment.getProperty("tunnel.server-host") + ".jtunnel.net");
+    tunnelConfig.setServerPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("tunnel.server-port"))));
+    tunnelConfig.setDestHost(environment.getProperty("tunnel.dest-host"));
+    tunnelConfig.setDestPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("tunnel.dest-port"))));
+    return tunnelConfig;
+  }
+
+  @Bean
+  public DataStore dataStore(Environment environment) {
+    return new MapDbDataStore(environment.getProperty("data-store-location"));
   }
 }
